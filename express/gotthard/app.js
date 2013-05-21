2b99cc225009ec354429fa0e4a51eedd9125d1c0
Lucas Chiesa <lucas.chiesa@gmail.com>
2013-05-21 18:04:46 -0300

    More experiments with my express app.

--- express/gotthard/app.js
+++ express/gotthard/app.js
@@ -29,7 +29,12 @@ app.configure('production', function(){
 
 // Routes
 
+function serve_start(req, res) {
+  res.render('start', {title:'Express', name:'Josefa'});
+};
+
 app.get('/', routes.index);
+app.get('/start', serve_start);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
